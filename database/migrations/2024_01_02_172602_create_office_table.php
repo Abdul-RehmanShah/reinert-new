786b40c87c1fbf4a30e46d71b3608700e68f4aba
Abdul Rehman Shah <83021773+Abdul-RehmanShah@users.noreply.github.com>
2024-01-20 22:30:51 +0100

    Update 2024_01_02_172602_create_office_table.php

--- database/migrations/2024_01_02_172602_create_office_table.php
+++ database/migrations/2024_01_02_172602_create_office_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('office', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('Floor_id');
-            $table->integer('Floor_id');
             $table->string('Name', 120);
             $table->double('size', 5, 2);
             $table->integer('NoOfRooms');
